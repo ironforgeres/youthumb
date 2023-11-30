ed290ddb9b2457dc6d39e07bbd2d41f106a3b079
mohamed ams <152399019+ironforgeres@users.noreply.github.com>
2023-11-30 15:44:42 +0100

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,43 @@
+// Import necessary modules and styles
 import "../styles/index.css";
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 
+// Define the MyApp component
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+      {/* Set up default SEO metadata */}
       <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
+        title="TikTok Video Downloader"
+        description="Download TikTok videos easily with our downloader."
         canonical="https://your-website-url.com"
         openGraph={{
           url: "https://your-website-url.com",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
+          title: "TikTok Video Downloader",
+          description: "Download TikTok videos easily with our downloader.",
+          site_name: "TikTok Video Downloader",
+          images: [
+            {
+              url: "https://your-website-url.com/your-thumbnail-image.jpg",
+              width: 1200,
+              height: 630,
+              alt: "Your Thumbnail Image Alt Text",
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@YourTwitterHandle',
+          site: '@YourTwitterHandle',
+          cardType: 'summary_large_image',
         }}
       />
+
+      {/* Render the main component */}
       <Component {...pageProps} />
     </Fragment>
   );
 }
 
+// Export the component
 export default MyApp;
