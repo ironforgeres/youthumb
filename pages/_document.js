7d98b5549fe27eb3fbed30370f3937ce4cfc497d
mohamed ams <152399019+ironforgeres@users.noreply.github.com>
2023-11-30 15:47:41 +0100

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// _document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
+          {/* Add external stylesheets or other head elements here */}
+          <link rel="stylesheet" href="https://your-website-url.com/external.css" />
+
+          {/* Set up metadata for social sharing */}
+          <meta property="og:title" content="TikTok Video Downloader" />
+          <meta property="og:description" content="Download TikTok videos easily with our downloader." />
+          <meta property="og:image" content="https://your-website-url.com/your-thumbnail-image.jpg" />
+          <meta property="og:url" content="https://your-website-url.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@YourTwitterHandle" />
+          <meta name="twitter:creator" content="@YourTwitterHandle" />
         </Head>
         <body>
           <Main />
